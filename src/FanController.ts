d1f997a44eadb467b17504d972ac4ad09d116412
Matthew Lyon <matthewdaniellyon@gmail.com>
2021-02-18 01:04:21 -0500

    converted some FanController methods to ts

--- src/FanController.ts
+++ src/FanController.ts
@@ -1,4 +1,43 @@
-class FanController {
+import EventEmitter from "events"
+import dgram from "dgram"
+
+class FanController extends EventEmitter {
+    readonly scan: boolean
+    public logTraffic: boolean
+    public discoveredFans: object = {}
+    private socket: dgram.Socket = dgram.createSocket("udp4")
+
+    static everyone: string = "255.255.255.255"
+    static port: number = 31415
+    static commandToFindFans: string[] = ["DEVICE", "ID", "GET"]
+
+    constructor(scan: boolean = true, logTraffic: boolean = false) {
+        super()
+        this.scan = scan
+        this.logTraffic = logTraffic
+        this.socket.bind(FanController.port, this.socketBound)
+    }
+
+    public send(query: string[], address: string) {
+        let command: string = `<${query.join(";")}>`
+        let payload = Buffer.from(command)
+        this.socket.send(payload, FanController.port, address, this.socketCallback)
+        if (this.logTraffic) console.log(`Outgoing: "${command}" => ${address}`)
+    }
+
+    public broadcast(query: string[]) {
+        query.unshift("ALL")
+        this.send(query, FanController.everyone)
+    }
+
+    private socketBound() {
+        this.socket.setBroadcast(true)
+        this.broadcast(FanController.commandToFindFans)
+    }
+
+    private socketCallback(error: Error | null, bytes: number) {
+        if (error !== null) throw error
+    }
 
 }
 
