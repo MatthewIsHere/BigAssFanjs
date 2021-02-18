9103e5c6c6ec54c82434d5eb8d90983834a17762
Matthew Lyon <matthewdaniellyon@gmail.com>
2021-02-18 13:23:09 -0500

    added msg handler, fixed persistent unshift() bug.

--- src/FanController.ts
+++ src/FanController.ts
@@ -1,5 +1,5 @@
 import EventEmitter from "events"
-import dgram from "dgram"
+import dgram, { RemoteInfo } from "dgram"
 
 class FanController extends EventEmitter {
     readonly scan: boolean
@@ -9,13 +9,14 @@ class FanController extends EventEmitter {
 
     static everyone: string = "255.255.255.255"
     static port: number = 31415
-    static commandToFindFans: string[] = ["DEVICE", "ID", "GET"]
+    static readonly commandToFindFans: string[] = ["DEVICE", "ID", "GET"]
 
     constructor(scan: boolean = true, logTraffic: boolean = false) {
         super()
         this.scan = scan
         this.logTraffic = logTraffic
-        this.socket.bind(FanController.port, this.socketBound)
+        this.socket.bind(FanController.port, this.socketBound.bind(this))
+        this.socket.on("message", this.socketMessage.bind(this))
     }
 
     public send(query: string[], address: string) {
@@ -26,8 +27,9 @@ class FanController extends EventEmitter {
     }
 
     public broadcast(query: string[]) {
-        query.unshift("ALL")
-        this.send(query, FanController.everyone)
+        let assembledQuery: string[] = Array.from(query)
+        assembledQuery.unshift("ALL")
+        this.send(assembledQuery, FanController.everyone)
     }
 
     public discover() {
@@ -38,12 +40,18 @@ class FanController extends EventEmitter {
         this.socket.setBroadcast(true)
         if (this.scan) {
             this.discover()
-            setInterval(this.discover, 5000)
+            //setInterval(this.discover.bind(this), 10000) maybe not necessary
         }
     }
 
     private socketCallback(error: Error | null, bytes: number) {
         if (error !== null) throw error
+    }
+
+    private socketMessage(payload: Buffer, sender: RemoteInfo) {
+        let message: string = String(payload)
+        if (!message.match(/^\(.*\)$/)) return
+        if (this.logTraffic) console.log(`Incoming: "${message}" <= ${sender.address}`)
     }
 
 }
