b449e32204124673a4fc2a2acbd0de796dc18542
Matthew Lyon <matthew@matthewlyon.net>
2021-03-04 22:28:47 -0500

    added json parsing for the command queries

--- src/BigAssFan.ts
+++ src/BigAssFan.ts
@@ -1,11 +1,22 @@
 import FanController from "./FanController";
+import { join } from "path"
+import { readFileSync } from "fs"
+
+interface booleanProperty {
+    (newValue?: boolean): Promise<boolean>;
+}
+interface numberPropery {
+    (newValue?: number): Promise<number>;
+}
+const queryJSONPath = join(__dirname, "queries.json")
+const queries = JSON.parse(String(readFileSync(queryJSONPath)))
 
 class BigAssFan {
     public readonly name
     public readonly mac
     private readonly ip
     private readonly controller 
-
+    
     constructor (name: string, mac: string, ip: string, controller: FanController) {
         this.name = name
         this.mac = mac
@@ -14,13 +25,25 @@ class BigAssFan {
     }
     
     public receiveMessage(query: string[]) {
-
+        console.log(query)
     }
 
     public send(query: string[]) {
         let assembledQuery = Array.from(query)
         assembledQuery.unshift(this.mac)
         this.controller.send(assembledQuery, this.ip)
+    }
+
+    speed(speed?: number): Promise<number> {
+        if (speed) {
+            
+        } else {
+
+        }
+        //registerListener()
+        return new Promise((resolve) => {
+            resolve(8)
+        })
     }
 }
 
