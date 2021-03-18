d1143a74fcafdc410fd6728213e13dd2832016ef
Matthew Lyon <matthew@matthewlyon.net>
2021-03-18 14:59:30 -0400

    setup message handling through promises. works well but could be better

--- src/BigAssFan.ts
+++ src/BigAssFan.ts
@@ -1,17 +1,19 @@
 import FanController from "./FanController";
 import { join } from "path"
 import { readFileSync } from "fs"
+import EventEmitter from "events";
 
 const queryJSONPath = join(__dirname, "queries.json")
 const queries = JSON.parse(String(readFileSync(queryJSONPath)))
 
-class BigAssFan {
+class BigAssFan extends EventEmitter {
     public readonly name
     public readonly mac
     private readonly ip
     private readonly controller 
     
     constructor (name: string, mac: string, ip: string, controller: FanController) {
+        super()
         this.name = name
         this.mac = mac
         this.ip = ip
@@ -19,7 +21,7 @@ class BigAssFan {
     }
     
     public receiveMessage(query: string[]) {
-        console.log(query)
+        this.emit("response", query)
     }
 
     public send(query: string[]) {
@@ -27,16 +29,31 @@ class BigAssFan {
         assembledQuery.unshift(this.mac)
         this.controller.send(assembledQuery, this.ip)
     }
-   /* private registerForResponse(query1: string, query2: string): Promise<> {
+   
+    private registerForResponse(query1: string, query2: string): Promise<string[]> {
+        let promise = new Promise(resolve => {
+            this.on("response", (response: string[]) => {
+                if (response[0] !== query1) return
+                if (response[1] !== query2) return
+                resolve(response)
+            })
+        })
+        return promise as Promise<string[]>
+    }
 
-    }*/
-    speed(speed?: number): void {
+    speed(speed?: number): Promise<Number> {
         let query: string[] = Array.from(queries.speed)
         let operationType = (speed == undefined) ? "GET": "SET"
         query.splice(2, 0, operationType)
         if(speed) query[3] = String(speed)
+        let returnPromise = new Promise(resolve => {
+            this.registerForResponse(query[0], query[1])
+            .then(response => {
+                resolve(Number(response[3]))
+            }) 
+        }) as Promise<Number>
         this.send(query)
-        //this is where we get the response
+        return returnPromise
     }
 }
 
